f081b1145797985eb528c190687c21e97e3f431c
QingYu <MQing@clarku.edu>
2019-11-11 20:55:32 -0500

    I just finished the loading screen and also change the app name and the icon

---
+++ app/src/main/java/com/example/aplustea/mainActivity/MainActivity.kt
@@ -0,0 +1,35 @@
+package com.example.aplustea.mainActivity
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupWithNavController
+import com.example.aplustea.R
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        setSupportActionBar(findViewById(R.id.toolbar))
+
+//        val navController = findNavController(R.id.nav_host_frag)
+//        toolbar.setupWithNavController(navController)
+
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu): Boolean {
+        menuInflater.inflate(R.menu.toolbar_menu, menu)
+        return true
+
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem?): Boolean {
+        return super.onOptionsItemSelected(item)
+    }
+
+}
---
+++ app/src/main/java/com/example/aplustea/mainActivity/SplashActivity.kt
@@ -0,0 +1,28 @@
+package com.example.aplustea.mainActivity
+
+import android.content.Intent
+import android.os.Bundle
+import androidx.appcompat.app.AppCompatActivity
+import com.example.aplustea.R
+import java.lang.Exception
+
+class SplashActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.splash_activity)
+
+        val background = object : Thread() {
+            override fun run() {
+                try {
+                    Thread.sleep(4000)
+                    val intent = Intent(baseContext, MainActivity::class.java)
+                    startActivity(intent)
+                } catch (e: Exception) {
+                    e.printStackTrace()
+                }
+            }
+        }
+        background.start()
+    }
+
+}
