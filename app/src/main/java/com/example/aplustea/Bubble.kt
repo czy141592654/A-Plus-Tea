a357609297f78080f52355ef78e085b4e0f60db6
ITSFILLUP <phinguyen@clarku.edu>
2019-11-20 21:35:09 -0500

    FIXED MAJOR BUGS AND DELETED MANY SCREENS, UI NEEDS UPDATING.

---
+++ app/src/main/java/com/example/aplustea/Bubble.kt
@@ -0,0 +1,3 @@
+package com.example.aplustea
+
+class Bubble(var image:Int, var name:String)
---
+++ app/src/main/java/com/example/aplustea/adapter/RecyclerViewAdapterBubble.kt
@@ -0,0 +1,43 @@
+package com.example.aplustea.adapter
+
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.recyclerview.widget.RecyclerView
+import com.example.aplustea.Bubble
+import com.example.aplustea.CartScreenItem
+import com.example.aplustea.R
+import kotlinx.android.synthetic.main.bubble_item.view.*
+import kotlinx.android.synthetic.main.cart_item.view.*
+import kotlinx.android.synthetic.main.fragment_cart_screen.view.*
+
+class RecyclerViewAdapterBubble(
+    var bubble_array: ArrayList<Bubble>
+) : RecyclerView.Adapter<RecyclerViewAdapterBubble.RecyclerViewHolder>() {
+
+    override fun onCreateViewHolder(
+        parent: ViewGroup,
+        viewType: Int
+    ): RecyclerViewHolder {
+        val viewItem =
+            LayoutInflater.from(parent.context).inflate(R.layout.bubble_item, parent, false)
+        return RecyclerViewHolder(viewItem)
+    }
+
+    override fun getItemCount(): Int {
+        return bubble_array.size
+    }
+
+
+    override fun onBindViewHolder(holder: RecyclerViewHolder, position: Int) {
+        holder.bind(bubble_array[position])
+    }
+
+    inner class RecyclerViewHolder(var viewItem: View) : RecyclerView.ViewHolder(viewItem) {
+
+        fun bind(item: Bubble) {
+            viewItem.bubble.setBackgroundResource(item.image)
+            viewItem.bubble.setText(item.name)
+        }
+    }
+}
